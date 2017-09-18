65853d29ee3f1b79b7debe9bbe35b763d6cc5edd
Moises Guillermo Lozada Albarran <MoisesMac@macs-mac-mini.local>
2017-09-18 09:31:18 -0400

    Initial Commit

---
+++ HTTP connection model wrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTTP connection model wrapper
+//
+//  Created by Moi's Mac on 18/9/17.
+//  Copyright © 2017 Moi's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
